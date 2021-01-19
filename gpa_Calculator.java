d2b9165712f7d2c0f5061371b10f182ac3616ff3
Unidentified <71221226+Unidentified539@users.noreply.github.com>
2021-01-18 21:08:13 -0600

    Update gpa_Calculator.java
    Quit function added

--- gpa_Calculator.java
+++ gpa_Calculator.java
@@ -2,72 +2,67 @@ import java.util.Scanner;
 
 public class gpa_Calculator {
 
+
     public static void main(String[] args) {
 
         Scanner sc  = new Scanner(System.in);
+        boolean quit=false;
 
-        System.out.println("Enter your grade average in percent form.");
-        int grade_Average = sc.nextInt();
+        while (!quit) {
+            System.out.println("Enter your grade average in percent form. Type 1010 to exit.");
+            double grade_Average = sc.nextDouble();
 
 
-        if (grade_Average <= 59) {
-            System.out.println("0.0 GPA");
+                if (grade_Average <= 59) {
+                    System.out.println("0.0 GPA");
+                } else if (grade_Average <= 62) {
+                    System.out.println("0.7 GPA");
 
-        }
+                } else if (grade_Average <= 63) {
+                    System.out.println("1.0 GPA");
 
-        else if (grade_Average <= 62) {
-            System.out.println("0.7 GPA");
+                } else if (grade_Average <= 69) {
+                    System.out.println("1.3 GPA");
 
-        }
+                } else if (grade_Average <= 72) {
+                    System.out.println("1.7 GPA");
+                } else if (grade_Average <= 76) {
+                    System.out.println("2.0 GPA");
 
-        else if (grade_Average <= 63) {
-            System.out.println("1.0 GPA");
+                } else if (grade_Average <= 79) {
+                    System.out.println("7.3 GPA");
 
-        }
+                } else if (grade_Average <= 82) {
+                    System.out.println("2.7");
 
-        else if (grade_Average <= 69) {
-            System.out.println("1.3 GPA");
+                } else if (grade_Average <= 86) {
+                    System.out.println("3.0 GPA");
 
-        }
+                } else if (grade_Average <= 89)
+                    System.out.println("3.3");
 
-        else if (grade_Average <= 72) {
-            System.out.println("1.7 GPA");
-        }
+                else if (grade_Average <= 92) {
+                    System.out.println("3.7 GPA");
 
-        else if (grade_Average <= 76) {
-            System.out.println("2.0 GPA");
+                } else if (grade_Average <= 100) {
+                    System.out.println("4.0 GPA");
+                }
 
-        }
+                else if (grade_Average == 1010) {
+                    System.out.println("Quiting...");
+                    quit = true;
+                }
 
-        else if (grade_Average <= 79) {
-            System.out.println("7.3 GPA");
+                else {
+                    System.out.println(" Invalid Input Please try again");
+                }
 
-        }
-
-        else if (grade_Average <= 82) {
-            System.out.println("2.7");
-
-        }
-
-        else if (grade_Average <= 86) {
-            System.out.println("3.0 GPA");
-
-        }
-
-        else if (grade_Average <= 89)
-            System.out.println("3.3");
-
-        else if (grade_Average <= 92) {
-            System.out.println("3.7 GPA");
 
         }
 
-        else if (grade_Average <= 100) {
-            System.out.println("4.0 GPA");
-        }
+
     }
 
 
-
-
 }
+
