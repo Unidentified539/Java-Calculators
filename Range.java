4901218273df302268aef7e29bd98ca212db88b9
Aziel Solomon <71221226+Unidentified539@users.noreply.github.com>
2021-01-30 14:46:32 -0600

    Update Range.java

--- Range.java
+++ Range.java
@@ -1,23 +1,43 @@
-package com.nihilo;
+package com.company;
 
 public class Range {
 
-    private double low;
-    private double high;
+    private int low;
+    private int high;
 
-    public Range(double low, double high) {
+    public Range(int low, int high) {
         this.low = low;
+        this.high = high;
+
+    }
+
+
+    public int getLow() {
+        return low;
+    }
+
+    public void setLow(int low) {
+        this.low = low;
+    }
+
+    public int getHigh() {
+        return high;
+    }
+
+    public void setHigh(int high) {
         this.high = high;
     }
 
- public Boolean contains(double input){
-        if(input>=low&&input<=high){
-            return true;
-        }
+    public boolean isInRange(int num){
+        return num >= low && num <= high;
+    }
+
+    public boolean isInRange(int num,int num2){
+        return num >= low && num2 <= high;
+    }
+
+
+    public boolean contains(double average) {
         return false;
- }
-
-
-
-
+    }
 }
