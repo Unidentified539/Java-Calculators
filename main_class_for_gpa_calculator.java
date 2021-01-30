adc108b6167e9570840550aeab98ddab758aff39
Aziel Solomon <71221226+Unidentified539@users.noreply.github.com>
2021-01-30 14:49:02 -0600

    Delete main_class_for_gpa_calculator.java
    Unnecessary.

--- main_class_for_gpa_calculator.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-class main_class_for_gpa_calculator {
-    public static void main(String[] args) {
-        gpa_Calculator function = new gpa_Calculator();
-        function.func();
-    }
-}
