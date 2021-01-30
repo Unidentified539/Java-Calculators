a49580c19db6b97b0b0a63796b218d51ae63b279
Aziel Solomon <71221226+Unidentified539@users.noreply.github.com>
2021-01-30 14:48:26 -0600

    Update gpa_Calculator.java
    Func.java but with 1010.

--- gpa_Calculator.java
+++ gpa_Calculator.java
@@ -1,68 +1,146 @@
+package com.company;
 // Main class can be found at Main_Class file in this repo
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
-public class gpa_Calculator {
+public class Calc {
 
-    public void func() {
-        Scanner sc  = new Scanner(System.in);
-        boolean quit=false;
+    HashMap<Range, String> func = new HashMap<>();
+    List<Range> box = new ArrayList<>();
 
-        while (!quit) {
-            System.out.println("Enter your grade average in percent form. Type 1010 to exit.");
-            double grade_Average = sc.nextDouble();
+    public static void new_File() throws IOException {
+        File myObj = new File("data.txt");
 
+    }
 
-            if (grade_Average <= 59) {
-                System.out.println("0.0 GPA");
-            } else if (grade_Average <= 62) {
-                System.out.println("0.7 GPA");
-
-            } else if (grade_Average <= 63) {
-                System.out.println("1.0 GPA");
-
-            } else if (grade_Average <= 69) {
-                System.out.println("1.3 GPA");
-
-            } else if (grade_Average <= 72) {
-                System.out.println("1.7 GPA");
-            } else if (grade_Average <= 76) {
-                System.out.println("2.0 GPA");
-
-            } else if (grade_Average <= 79) {
-                System.out.println("7.3 GPA");
-
-            } else if (grade_Average <= 82) {
-                System.out.println("2.7");
-
-            } else if (grade_Average <= 86) {
-                System.out.println("3.0 GPA");
-
-            } else if (grade_Average <= 89)
-                System.out.println("3.3");
-
-            else if (grade_Average <= 92) {
-                System.out.println("3.7 GPA");
-
-            } else if (grade_Average <= 100) {
-                System.out.println("4.0 GPA");
+    public static void read_File() {
+        try {
+            Scanner sc = new Scanner(System.in);
+            File myObj = new File("data.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println("Your previous grade average was " + data);
+                System.out.println("Would you like to erase your previous grade average? y/n");
+                String y_n = sc.nextLine();
+                if (y_n.equals("y")) {
+                    delete_File();
+                } else if (y_n.equals("n")) {
+                    System.out.println("okay");
+                } else {
+                    System.out.println("Invalid response Enter y or n.");
+                }
             }
-
-            else if (grade_Average == 1010) {
-                System.out.println("Quiting...");
-                quit = true;
-            }
-
-            else {
-                System.out.println(" Invalid Input Please try again");
-            }
-
-
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
 
 
+    public static String write_File(double Write_This) {
+        try {
+            FileWriter myWriter = new FileWriter("data.txt");
+            myWriter.write(Double.toString(Write_This));
+            myWriter.close();
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        return "...";
+    }
+
+
+    public static void delete_File() {
+        File myObj = new File("data.txt");
+        if (myObj.delete()) {
+            System.out.println();
+        } else {
+            System.out.println("Failed to delete the file.");
+        }
+
 
     }
 
 
+    public void menu() throws IOException {
 
+        Range class1 = new Range(0, 64);
+        Range class2 = new Range(65, 66);
+        Range class3 = new Range(67, 69);
+        Range class4 = new Range(70, 72);
+        Range class5 = new Range(73, 76);
+        Range class6 = new Range(77, 79);
+        Range class7 = new Range(80, 82);
+        Range class8 = new Range(83, 86);
+        Range class9 = new Range(87, 89);
+        Range class10 = new Range(90, 92);
+        Range class11 = new Range(93, 100);
+        func.put(class1, "0.0 GPA");
+        func.put(class2, "1.0 GPA");
+        func.put(class3, "1.3 GPA");
+        func.put(class4, "1.7 GPA");
+        func.put(class5, "2.0 GPA");
+        func.put(class6, "2.3 GPA");
+        func.put(class7, "2.7 GPA");
+        func.put(class8, "3.0 GPA");
+        func.put(class9, "3.3 GPA");
+        func.put(class10, "3.7 GPA");
+        func.put(class11, "4.0 GPA");
+        box.add(class1);
+        box.add(class2);
+        box.add(class3);
+        box.add(class4);
+        box.add(class5);
+        box.add(class6);
+        box.add(class7);
+        box.add(class8);
+        box.add(class9);
+        box.add(class10);
+        box.add(class11);
+
+        Scanner sc = new Scanner(System.in);
+        boolean quit = false;
+
+
+        while (!quit) {
+            new_File();
+            read_File();
+            System.out.println("Enter your grade average in percent form. Type 1010 to exit.");
+            int grade_Average = (int) sc.nextDouble();
+            System.out.println(getGpa(grade_Average));
+            if (grade_Average == 1010) {
+                quit = true;
+            }
+
+            write_File(grade_Average);
+        }
+    }
+
+    //so i created an arrayList and added all the ranges so that we can go through the file and check if its with in the range
+    //if its with in the range it returns true then we assign that range to tmp and use it as a key for our hash map then we  return
+    //the value of the hash map which is a string if its not in there we return a string that says enter a valid number
+    public String getGpa(double average) {
+        for (int i = 0; i < box.size(); i++) {
+            Range tmp = box.get(i);
+            if (tmp.contains(average)) {
+                return func.get(tmp);
+            }
+
+
+        }
+        return "";
+
+    }
+
+}
